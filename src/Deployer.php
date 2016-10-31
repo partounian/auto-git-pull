2b3c931a139426b5c866e2a2004263521f501158
Patrick Artounian <partounian@gmail.com>
2016-10-30 17:21:02 -0700

    Replace redundant dirname(__DIR__) with just __DIR__

--- src/Deployer.php
+++ src/Deployer.php
@@ -124,7 +124,7 @@ class Deployer {
         }
 
         if (empty($this->pullScriptPath)) {
-            $this->pullScriptPath = dirname(__DIR__) . '/scripts/git-pull.sh';
+            $this->pullScriptPath = __DIR__ . '/scripts/git-pull.sh';
         }
     }
 
